1b1d06a07aaf459c14826a5376d9d1d7f12ecab8
Stian Selnes <stian@pexip.com>
2018-09-27 16:20:16 +0200

    gthread: Optimize g_once_init_enter() for non __GNUC__
    Optimize the most common where value_location is already set,
    so that we don't have to take the lock. This is similar to
    what's done with macros for __GNUC__.

--- glib/gthread.c
+++ glib/gthread.c
@@ -702,6 +702,12 @@ gboolean
 {
   gsize *value_location = (gsize *) location;
   gboolean need_init = FALSE;
+
+#ifndef __GNUC__
+  if (g_atomic_pointer_get (value_location) != NULL)
+    return FALSE;
+#endif
+
   g_mutex_lock (&g_once_mutex);
   if (g_atomic_pointer_get (value_location) == 0)
     {
